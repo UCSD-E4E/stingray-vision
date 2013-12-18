0ca5cfbaf44fdd76d3024e7f252ecb82a17c1903
PNaughton <naughton.perry@gmail.com>
2013-12-17 16:52:45 -0800

    new catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hammerhead-vision/stingray-vision/src
+# Build directory: /home/hammerhead-vision/stingray-vision/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
